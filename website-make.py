6ae94fc9e5e6beea9478413d4d06078781d7e722
瞿相荣 <woshiquxiangrong@outlook.com>
2025-03-25 23:27:08 +0800

    update

--- website-make.py
+++ website-make.py
@@ -13,7 +13,7 @@ paths = os.walk('./')
 for path, tmp2, file_list in paths:
     for i in file_list:
         if i.endswith('.md'):
-            html_path = i[0:-3] + '.html'
+            html_path = 'stamon_'+i[0:-3] + '.html'
             fin = open(os.path.join(path, i), 'r', encoding='utf8')
             fout = open(os.path.join(path, html_path), 'w', encoding='utf8')
             fout.write('''<!DOCTYPE html>
@@ -24,16 +24,16 @@ for path, tmp2, file_list in paths:
     <!-- 新 Bootstrap4 核心 CSS 文件 -->
     <link
         rel="stylesheet"
-        href="https://cdn.staticfile.org/twitter-bootstrap/4.3.1/css/bootstrap.min.css"
+        href="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css"
     />
     <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
-    <script src="https://cdn.staticfile.org/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
     <!-- bootstrap.bundle.min.js 用于弹窗、提示、下拉菜单，包含了 popper.min.js -->
-    <script src="https://cdn.staticfile.org/popper.js/1.15.0/umd/popper.min.js"></script>
+    <script src="https://cdn.bootcdn.net/ajax/libs/popper.js/1.15.0/umd/popper.min.js"></script>
 
     <!-- 最新的 Bootstrap4 核心 JavaScript 文件 -->
-    <script src="https://cdn.staticfile.org/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
     </head>
 
     <body>
@@ -56,8 +56,8 @@ for path, tmp2, file_list in paths:
         </li>
         </ul>
     </nav>
-    <br>'''
+    <br><div style=\"margin: auto;\">'''
                     +markdown.markdown(fin.read())
-                    +'''  </body>
+                    +''' </div> </body>
     </html>'''
             )
